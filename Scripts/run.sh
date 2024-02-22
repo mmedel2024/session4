31a8637d518c01dd5197aba62d3ca50f557055c0
mmedel@upexternos.com.mx <mmedel@upexternos.com.mx>
2024-02-22 23:59:44 +0000

    actualizacion de datos en el archivo run

--- Scripts/run.sh
+++ Scripts/run.sh
@@ -6,5 +6,5 @@ docker stop test_api01 || true
 # Run container application
 #docker run -itd --name test_api01 -p 3001:3000 --rm app:1.0 
 #docker run -itd --name test_api01 -p 3001:3000 --rm app:$1
-docker run -itd --name test_api01 -p 3001:3000 --rm rogermz/api_cdk_ene24:$1
+docker run -itd --name test_api01 -p 3001:3000 --rm mmedel2024/session4:$1
 
